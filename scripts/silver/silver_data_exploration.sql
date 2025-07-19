1b26ac166a0536a146302725b37d85191ce853f1
Emre Kandir <75427107+kyem404@users.noreply.github.com>
2025-07-19 16:56:06 +0300

    Update silver_data_exploration.sql

--- scripts/silver/silver_data_exploration.sql
+++ scripts/silver/silver_data_exploration.sql
@@ -1,6 +1,5 @@
 -- Check for Nulls or Duplicates in the Primary Key
 -- Expectation: No Result
-
 SELECT
 cst_id,
 count(*)
@@ -9,23 +8,12 @@ GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
 
--- Transformation to keep only the unique primary keys
-select
-*
-from(
-
-select
-*,
-row_number() over (partition by cst_id order by cst_create_date desc) flag_last
-from bronze.crm_cust_info
-)t where flag_last = 1;
-
-
 -- Check for unwanted spaces in First Names
 -- Expectation: No Results
 select cst_firstname
 from bronze.crm_cust_info
 where cst_firstname != trim(cst_firstname);
+
 
 -- Check for unwanted spaces in Last Names
 -- Expectation: No Results
