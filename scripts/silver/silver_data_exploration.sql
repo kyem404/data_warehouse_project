5d8459d8df58bbeb7b709da79f2b700feb70acc9
Emre Kandir <75427107+kyem404@users.noreply.github.com>
2025-07-19 17:18:40 +0300

    Update silver_data_exploration.sql

--- scripts/silver/silver_data_exploration.sql
+++ scripts/silver/silver_data_exploration.sql
@@ -1,3 +1,7 @@
+----------------------------------------------------------------------------
+-- Data exploration on the bronze table in order to create the transformations upon inserting into the silver table.
+----------------------------------------------------------------------------
+
 -- Check for Nulls or Duplicates in the Primary Key
 -- Expectation: No Result
 SELECT
@@ -34,3 +38,45 @@ FROM bronze.crm_cust_info;
 -- Check for Data Standardization & Consistency in cst_marital_status
 SELECT DISTINCT cst_marital_status
 FROM bronze.crm_cust_info;
+
+----------------------------------------------------------------------------
+-- Quality Checks for the SILVER table after being inserted transformed data
+----------------------------------------------------------------------------
+
+
+-- Check for Nulls or Duplicates in the Primary Key
+-- Expectation: No Result
+SELECT
+cst_id,
+count(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+
+-- Check for unwanted spaces in First Names
+-- Expectation: No Results
+select cst_firstname
+from silver.crm_cust_info
+where cst_firstname != trim(cst_firstname);
+
+
+-- Check for unwanted spaces in Last Names
+-- Expectation: No Results
+select cst_lastname
+from silver.crm_cust_info
+where cst_lastname != trim(cst_lastname);
+
+-- Check for unwanted spaces in Gender
+-- Expectation: No Results
+select cst_gndr
+from silver.crm_cust_info
+where cst_gndr != trim(cst_gndr);
+
+-- Check for Data Standardization & Consistency in cst_gndr
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info;
+
+-- Check for Data Standardization & Consistency in cst_marital_status
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info;
