49061b08263b6dc834c5d01e4cd7f142241318f1
Emre Kandir <75427107+kyem404@users.noreply.github.com>
2025-07-19 16:56:17 +0300

    Update silver_data_transformation.sql

--- scripts/silver/silver_data_transformation.sql
+++ scripts/silver/silver_data_transformation.sql
@@ -1,1 +1,10 @@
+-- Transformation to keep only the unique primary keys
+select
+*
+from(
 
+select
+*,
+row_number() over (partition by cst_id order by cst_create_date desc) flag_last
+from bronze.crm_cust_info
+)t where flag_last = 1;
