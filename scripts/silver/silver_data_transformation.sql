e9c15253b0e5211d71319aa3f547ff39cb8a79c1
Emre Kandir <75427107+kyem404@users.noreply.github.com>
2025-07-19 17:25:42 +0300

    Update silver_data_transformation.sql

--- scripts/silver/silver_data_transformation.sql
+++ scripts/silver/silver_data_transformation.sql
@@ -1,4 +1,4 @@
--- Transformation to keep only the unique primary keys
+-- Data Transformation processes of the table bronze.crm_cust_info to silver.crm_cust_info
 INSERT INTO silver.crm_cust_info (
 	cst_id,
 	cst_key,
@@ -11,16 +11,16 @@ INSERT INTO silver.crm_cust_info (
 SELECT
 	cst_id,
 	cst_key,
-	TRIM(cst_firstname) AS cst_firstname,
-	TRIM(cst_lastname) AS cst_lastname,
+	TRIM(cst_firstname) AS cst_firstname, -- Removed trailing spaces for First Names
+	TRIM(cst_lastname) AS cst_lastname, -- Removed trailing spaces for Last Names
 	CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
 		 WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
 		 ELSE 'n/a'
-	END cst_marital_status,
+	END cst_marital_status, -- Normalize marital status values to readable format
 	CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
 		 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
 		 ELSE 'n/a'
-	END cst_gndr,
+	END cst_gndr, -- Normalize gender values to readable format
 	cst_create_date
 FROM(
 	SELECT
@@ -28,4 +28,4 @@ FROM(
 		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) flag_last
 	FROM bronze.crm_cust_info
 	WHERE cst_id IS NOT NULL
-)t WHERE flag_last = 1;
+)t WHERE flag_last = 1; -- Selected the most recent record per customer
