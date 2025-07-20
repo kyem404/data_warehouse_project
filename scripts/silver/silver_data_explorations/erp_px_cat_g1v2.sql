b112b4afc38e4fc5760504c8f9e8599585600d85
Emre Kandir <75427107+kyem404@users.noreply.github.com>
2025-07-20 21:27:18 +0300

    Create erp_px_cat_g1v2.sql

---
+++ scripts/silver/silver_data_explorations/erp_px_cat_g1v2.sql
@@ -0,0 +1,21 @@
+-- Data exploration for the bronze layer
+-- Check for unwanted Spaces
+SELECT * FROM bronze.erp_px_cat_g1v2
+WHERE cat != TRIM(cat) 
+OR subcat != TRIM(subcat)
+OR maintenance != TRIM(maintenance);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+cat
+FROM bronze.erp_px_cat_g1v2;
+
+SELECT DISTINCT
+subcat
+FROM bronze.erp_px_cat_g1v2;
+
+SELECT DISTINCT
+maintenance
+FROM bronze.erp_px_cat_g1v2;
+
+-- No need for further quality checks as the data is good of quality.
