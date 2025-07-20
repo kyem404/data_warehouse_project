99a6f7ab2e5490704b7e2689ce2177959962d2a0
Emre Kandir <75427107+kyem404@users.noreply.github.com>
2025-07-20 18:37:14 +0300

    Update silver_data_exploration.sql

--- scripts/silver/silver_data_exploration.sql
+++ scripts/silver/silver_data_exploration.sql
@@ -1,6 +1,8 @@
 ----------------------------------------------------------------------------
--- Data exploration on the bronze table in order to create the transformations upon inserting into the silver table.
+-- Data exploration on the BRONZE tables in order to create the transformations upon inserting into the silver table.
 ----------------------------------------------------------------------------
+
+-- bronze.crm_cust_info
 
 -- Check for Nulls or Duplicates in the Primary Key
 -- Expectation: No Result
@@ -42,8 +44,10 @@ FROM bronze.crm_cust_info;
 
 
 ----------------------------------------------------------------------------
--- Quality Checks for the SILVER table after being inserted transformed data
+-- Quality Checks for the SILVER tables after being inserted transformed data
 ----------------------------------------------------------------------------
+
+-- silver.crm_cust_info
 
 -- Check for Nulls or Duplicates in the Primary Key
 -- Expectation: No Result
@@ -81,3 +85,49 @@ FROM silver.crm_cust_info;
 -- Check for Data Standardization & Consistency in cst_marital_status
 SELECT DISTINCT cst_marital_status
 FROM silver.crm_cust_info;
+
+----------------------------------------------------------------------------
+
+-- bronze.crm_prd_info
+
+-- Check for Nulls or Duplicates in the Primary Key
+-- Expectation: No Result
+-- Check for Nulls or Duplicates in the Primary Key
+-- Expectation: No Result
+SELECT
+prd_id,
+count(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for unwanted spaces in string columns
+-- Expectation: No Results
+select prd_nm
+from bronze.crm_prd_info
+where prd_nm != trim(prd_nm);
+
+-- Check for NULLs or Negative Numbers
+-- Expectation: No Results
+select prd_cost
+from bronze.crm_prd_info
+where prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standardization & Consistency
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info;
+
+-- Check for Invalid Date Orders
+SELECT * 
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
+
+
+
+
+
+
+
+
+
+---
